49bfbc98d3b9b211a636795c381a5236858e7fcb
himanshu <himanshusharmahs974@gmail.com>
2016-08-14 20:49:42 +0530

    To calculate sqaure root and cube root

---
+++ sqaurecube.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class sqaurecube{
+public static void main(String[] args){
+System.out.println("enter intezer to solve cube and square root");
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+System.out.println("You number is :" + num);
+System.out.println("square root is :" + Math.sqrt(num));
+System.out.println("cube root is :" + Math.cbrt(num));
+}}
+
